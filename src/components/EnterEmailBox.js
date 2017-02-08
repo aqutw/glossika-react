7ea5830ac8bfca945d869f3da56cbee89c0b41eb
aqutw <aquarian.ex+github@gmail.com>
2017-02-08 22:19:03 +0800

    Ready EnterEmailBox.js component

--- src/components/EnterEmailBox.js
+++ src/components/EnterEmailBox.js
@@ -27,13 +27,13 @@ export class EnterEmailBox extends React.Component {
     </header>
     <section className="form-container">
         <form id="mailchimp-form">
-            <input type="hidden" name="csrftoken" value="PXo7Xbws-VhFtQw7NI5KZmCdJMAcOMom6Gx8">
-            <input type="hidden" name="list_id" value="300474d086">
+            <input type="hidden" name="csrftoken" value="PXo7Xbws-VhFtQw7NI5KZmCdJMAcOMom6Gx8" />
+            <input type="hidden" name="list_id" value="300474d086" />
             <p id="form-error"></p>
             
                 <p></p><p>Still searching for the <strong>best proven method</strong> to tackle a new language? We have the answer for you <i className="fa fa-smile-o"></i></p><p>Download our PDF now and get in on the secret methods used by polyglots!</p><p></p>
                 <label for="email">Enter your email:</label>
-                <input type="email" name="email" placeholder="john@example.com" pattern="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$" required="">
+                <input type="email" name="email" placeholder="john@example.com" pattern="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$" required="" />
                 <label className="fineprint">I want to register at glossika.com and subscribe to the Glossika Newsletter.</label>
             
             <button type="submit" id="mailchimp-submit">Subscribe</button>
